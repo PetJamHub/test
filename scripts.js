daaaf7887dcad7210cad99b0f53c4de3577192f5
ragdoll1es <130092104+ragdoll1es@users.noreply.github.com>
2023-04-06 15:16:14 -0400

    Add files via upload

--- scripts.js
+++ scripts.js
@@ -1,4 +1,13 @@
 
+document.querySelectorAll('a[href^="#"]').forEach(anchor => {
+    anchor.addEventListener('click', function (e) {
+        e.preventDefault();
+        document.querySelector(this.getAttribute('href')).scrollIntoView({
+            behavior: 'smooth'
+        });
+    });
+});
+
 document.addEventListener('DOMContentLoaded', function() {
     const contactForm = document.getElementById('contact-form');
 
