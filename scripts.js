8556c49801b3805d51036427a874e76c421c33bc
petjam_hub <130092104+petjamhub@users.noreply.github.com>
2023-04-06 23:52:44 -0400

    v0.9.0.4

--- scripts.js
+++ scripts.js
@@ -20,8 +20,60 @@ document.addEventListener('DOMContentLoaded', function() {
     });
 });
 
+// Set the date and time to count down to
+var countDownDate = new Date("April 30, 2023 00:00:00").getTime();
+
+// Update the countdown every second
+var x = setInterval(function() {
+
+  // Get the current date and time
+  var now = new Date().getTime();
+
+  // Calculate the distance between now and the countdown date
+  var distance = countDownDate - now;
+
+  // Calculate days, hours, minutes, and seconds remaining
+  var days = Math.floor(distance / (1000 * 60 * 60 * 24));
+  var hours = Math.floor((distance % (1000 * 60 * 60 * 24)) / (1000 * 60 * 60));
+  var minutes = Math.floor((distance % (1000 * 60 * 60)) / (1000 * 60));
+  var seconds = Math.floor((distance % (1000 * 60)) / 1000);
+
+  // Display the countdown timer
+  document.getElementById("days").innerHTML = days;
+  document.getElementById("hours").innerHTML = hours;
+  document.getElementById("minutes").innerHTML = minutes;
+  document.getElementById("seconds").innerHTML = seconds;
+
+  // If the countdown is finished, display a message
+  if (distance < 0) {
+    clearInterval(x);
+    document.getElementById("countdown").innerHTML = "Launched!";
+  }
+}, 1000);
 
 
-  
-  
-  
+var mybutton = document.getElementById("myBtn");
+
+// When the user scrolls down 20px from the top of the document, show the button
+window.onscroll = function() {
+  scrollFunction();
+};
+
+function scrollFunction() {
+  if (document.body.scrollTop > 20 || document.documentElement.scrollTop > 20) {
+    mybutton.style.display = "block";
+  } else {
+    mybutton.style.display = "none";
+  }
+}
+
+// When the user clicks on the button, scroll to the top of the document
+function topFunction() {
+  // Smooth scroll effect
+  var currentScroll = document.documentElement.scrollTop || document.body.scrollTop;
+  if (currentScroll > 0) {
+    window.requestAnimationFrame(topFunction);
+    window.scrollTo(0, currentScroll - (currentScroll / 8));
+  }
+}
+
